922668c2e64dddb85082595387eb356788a3cb84
NN-ACDM <nn.acdm@gmail.com>
2024-12-10 23:41:34 +0700

    [do filter] fix active twice

--- src/main/java/com/tsad/web/backend/config/FilterConfig.java
+++ src/main/java/com/tsad/web/backend/config/FilterConfig.java
@@ -1,21 +1,21 @@
-package com.tsad.web.backend.config;
-
-import com.tsad.web.backend.config.handler.RequestFilterHandler;
-import com.tsad.web.backend.service.authentication.CredentialService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class FilterConfig {
-
-    private final CredentialService credentialService;
-
-    public FilterConfig(CredentialService credentialService) {
-        this.credentialService = credentialService;
-    }
-
-    @Bean
-    public RequestFilterHandler customRequestFilter() {
-        return new RequestFilterHandler(credentialService);
-    }
-}
+//package com.tsad.web.backend.config;
+//
+//import com.tsad.web.backend.config.handler.RequestFilterHandler;
+//import com.tsad.web.backend.service.authentication.CredentialService;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class FilterConfig {
+//
+//    private final CredentialService credentialService;
+//
+//    public FilterConfig(CredentialService credentialService) {
+//        this.credentialService = credentialService;
+//    }
+//
+//    @Bean
+//    public RequestFilterHandler customRequestFilter() {
+//        return new RequestFilterHandler(credentialService);
+//    }
+//}
