7a91d5a9c001a943b622d66d609d6e8d14560788
NN-ACDM <nn.acdm@gmail.com>
2025-06-30 10:17:17 +0700

    [RL1] add swagger fix remove default

--- src/main/java/com/tsad/web/backend/Application.java
+++ src/main/java/com/tsad/web/backend/Application.java
@@ -3,10 +3,12 @@ package com.tsad.web.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@ComponentScan(basePackages = {"com.tsad.web.backend"})
 public class Application {
 
 	public static void main(String[] args) {
